6e682e8a6ead79dd4c25741d51752a0feda66816
Guilherme Golfetto <guilherme.golfetto@hotmail.com>
2024-05-31 18:41:58 -0300

    Initial Commit

---
+++ GloboPlayDesafio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GloboPlayDesafio
+//
+//  Created by Guilherme Golfetto on 31/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GloboPlayDesafio/GloboPlayDesafioApp.swift
@@ -0,0 +1,17 @@
+//
+//  GloboPlayDesafioApp.swift
+//  GloboPlayDesafio
+//
+//  Created by Guilherme Golfetto on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct GloboPlayDesafioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
